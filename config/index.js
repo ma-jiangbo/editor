f23d5d4289c9af2b54d2a7d0d66da3dec69caecb
majx <ma021024@126.com>
2018-01-15 15:03:03 +0800

    add

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/resumer/dist',
+    assetsPublicPath: '/jrg-project-5/resumer/dist',
 
     /**
      * Source Maps
